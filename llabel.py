fe9790060537cb4d06c3513b2b13f239b6862757
DevaGaneshB <40600219+DevaGaneshB@users.noreply.github.com>
2018-07-15 18:13:16 +0530

    Add files via upload

---
+++ llabel.py
@@ -0,0 +1,16 @@
+
+from Tkinter import *
+
+top = Tk()
+L1 = Label(top, text="User Name")
+L1.pack( side = LEFT)
+E1 = Entry(top, bd =5)
+E1.pack(side = RIGHT)
+
+
+L2 = Label(top, text="Password")
+L2.pack( side = LEFT)
+E2 = Entry(top, bd =5)
+E2.pack(side = RIGHT)
+top.mainloop()
+
