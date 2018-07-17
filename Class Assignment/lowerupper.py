bc7059da907b5800332e64ae09038e7fcb78492b
DevaGaneshB <40600219+DevaGaneshB@users.noreply.github.com>
2018-07-17 17:24:25 +0530

    Add files via upload

---
+++ Class Assignment/lowerupper.py
@@ -0,0 +1,13 @@
+
+def upplow(a):
+    up=0
+    lw=0
+    for i in range(len(a)):
+        if a[i].isupper():
+            up=up+1
+        if a[i].islower():
+            lw=lw+1
+    print("No. of uppercase ="+str(up))
+    print("No. of lowercase ="+str(lw))
+a=raw_input("Enter a string")
+upplow(a)
